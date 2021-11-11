0a8165e9e92c2bd929f138285dfce8d39798e58d
LautaroDiharce <lautarodiharce@gmail.com>
2021-11-11 12:43:11 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,14 +20,12 @@ import { ArticulosComponent } from './components/articulos/articulos.component';
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
       { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
-      { path: '**', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'articulos', component: ArticulosComponent },
       { path: '**', redirectTo: '/inicio', pathMatch: 'full' },
     ]),
   ],
   declarations: [
     AppComponent,
-    HelloComponent,
     InicioComponent,
     ArticulosFamiliasComponent,
     MenuComponent,
